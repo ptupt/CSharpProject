dd4a2913ede213a1d5ecc6c2cd948959bf730932
ptupt <ebaltebqvrot@gmail.com>
2026-02-25 15:37:52 +0300

    Добавлен базовый код вычисления y = 5x + 20

---
+++ MyApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число x:");
+            string input = Console.ReadLine();
+            
+            if (int.TryParse(input, out int x))
+            {
+                int y = 5 * x + 20;
+                Console.WriteLine($"Результат: y = {y}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите целое число!");
+            }
+            
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
