be71e4f3679acbf345513672bae7efb219751b8c
ptupt <ebaltebqvrot@gmail.com>
2026-02-25 15:46:20 +0300

    Задание готово к проверке

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,17 +8,18 @@ namespace MyApp
         {
             Console.WriteLine("Введите целое число x:");
             string input = Console.ReadLine();
-            
+
             if (int.TryParse(input, out int x))
             {
                 int y = 5 * x + 20;
-                Console.WriteLine($"Результат: y = {y}");
+                int result = y * 2; // Увеличиваем в 2 раза
+                Console.WriteLine($"Результат (увеличенный в 2 раза): {result}");
             }
             else
             {
                 Console.WriteLine("Ошибка: введите целое число!");
             }
-            
+
             Console.WriteLine("Нажмите любую клавишу для выхода...");
             Console.ReadKey();
         }
